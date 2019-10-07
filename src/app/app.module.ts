b197de900a637d7731945dee3827f2f7348e3d40
ABDELOUAHAD <mustapha.abdelouahad@capgemini.com>
2019-10-07 16:23:06 +0200

    Add two-way binding via FormModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppareilComponent } from './components/appareil/appareil.component';
 
+import { FormsModule } from "@angular/forms";
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { AppareilComponent } from './components/appareil/appareil.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
